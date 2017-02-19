94d08ffeeffaba191a9d477c8971e603d174d1e8
Kambiz Darabi <darabi@m-creations.net>
2017-02-19 19:08:46 +0100

    Remove unused property log4j.sym.home

--- docs/run-velocity-renderer.sh
+++ docs/run-velocity-renderer.sh
@@ -2,7 +2,7 @@
 
 #-agentlib:jdwp=transport=dt_socket,suspend=y,address=localhost:40709 
 
-$JAVA_HOME/bin/java -Dlog4j.sym.home=/tmp/ \
+$JAVA_HOME/bin/java \
 -Dfile.encoding=UTF-8 \
 -jar ~/.m2/repository/com/mcreations/renderer/velocity-renderer/1.0-SNAPSHOT/velocity-renderer-1.0-SNAPSHOT-jar-with-dependencies.jar \
 -s ./src -f .*.vm -d /tmp/
